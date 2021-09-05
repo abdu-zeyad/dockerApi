3009db0c5efe35d6f07d7f14c95487613e52f053
Abdelmajed-zeyad <abd.d@live.com>
2021-09-05 16:47:18 +0300

    add read me

--- api_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import EmployeesDetail, EmployeesList
-
-
-urlpatterns = [
-    path("", EmployeesList.as_view(), name="api_list"),
-    path("<int:pk>/", EmployeesDetail.as_view(), name=""),
-]
